8f34e3bf56b0f377f1d02854bb96479891dcf753
NotLord <amir01zx@gmail.com>
2025-11-12 09:40:31 +0200

    e

--- scripts/map_level_node.gd
+++ scripts/map_level_node.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 @export var level : Level
 @export var connected_levels : Array[MapLevelNode]
+@export var wireless_connected_levels : Array[MapLevelNode]
 @export var visible_by_def := false
 
 var lines : Dictionary[MapLevelNode, Line2D] = {}
@@ -11,10 +12,16 @@ var lines : Dictionary[MapLevelNode, Line2D] = {}
 
 func _ready() -> void:
 	_update()
+	
 	for conn in connected_levels:
 		if not conn.connected_levels.has(self):
 			conn.connected_levels.append(self)
+	for conn in wireless_connected_levels:
+		if not conn.wireless_connected_levels.has(self):
+			conn.wireless_connected_levels.append(self)
+	
 	await get_tree().process_frame
+	
 	_create_lines()
 
 
@@ -37,4 +44,6 @@ func _update() -> void:
 	visible = Global.completed_levels.has(name) or visible_by_def
 	if not visible:
 		for conn in connected_levels:
+			visible = visible or Global.completed_levels.has(conn.name)
+		for conn in wireless_connected_levels:
 			visible = visible or Global.completed_levels.has(conn.name)
